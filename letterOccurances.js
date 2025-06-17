171aad31565cfcee049a0ad066b9a9674d43fa60
swathiwohlig <swathi.cherala@wohlig.com>
2025-06-17 15:38:59 +0530

    letter occurance

---
+++ letterOccurances.js
@@ -0,0 +1,12 @@
+function wordOccurances(inputString){
+    let result = {}
+    for(let char of inputString){
+        if (result[char]){
+            result[char]++
+        } else{
+            result[char] = 1
+        }
+    }
+    return result
+}
+console.log(wordOccurances('aabacabbbbd'))
