37f371e8d606c93f9d411274211c14c84a02605d
swathicherala <86816430+swathicherala@users.noreply.github.com>
2025-07-01 13:57:02 +0530

    Update Anagrams.js

--- Anagrams.js
+++ Anagrams.js
@@ -1,12 +1,38 @@
-function isAnagram(input1,input2){
-    let splittedString = input2.split('')
-    for(let i=0; i<input1.length; i++){
-        if(splittedString.length != input1.length || !splittedString.includes(input1[i])){
+const isAnagram = (str1, str2) =>{
+    // Convert to lowercase and remove spaces
+  str1 = str1.toLowerCase().replace(/\s/g, '');
+  str2 = str2.toLowerCase().replace(/\s/g, '');
+
+  // If lengths don't match, they can't be anagrams
+  if (str1.length !== str2.length) return false
+    let result = {}
+    for(let i=0; i<str1.length; i++){
+       let char = str1[i]
+       if(result[char]){
+        result[char] += 1
+       }else{
+        result[char] = 1
+       }
+    }
+    for(let i=0; i<str2.length; i++){
+        let char = str2[i]
+       if(!result[char]){
+        return false
+       }
+       result[char]--
+    }
+
+    for(let key in result){
+        if(result[key] !== 0){
             return false
         }
     }
     return true
+
 }
+console.log(isAnagram('listen','silent'))
+console.log(isAnagram('listenp','silents'))
+console.log(isAnagram('hello','world'))
 console.log(isAnagram('cat','act'))
 console.log(isAnagram('cat','acts'))
 console.log(isAnagram('elbow','below'))
